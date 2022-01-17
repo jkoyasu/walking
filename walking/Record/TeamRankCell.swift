8a2cdd5e8402f81104d1ec0732a87f7c2b4bb695
yousukeHasegawa <hasegawa.yousuke.mn@mitsubishichem-sys.co.jp>
2022-01-17 11:31:29 +0900

    20220117時点

---
+++ walking/Record/TeamRankCell.swift
@@ -0,0 +1,23 @@
+//
+//  TeamRankCell.swift
+//  walking
+//
+//  Created by Yousuke Hasegawa on 2022/01/14.
+//
+
+import UIKit
+
+class TeamRankCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
