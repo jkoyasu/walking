f0b52eec9567cda79997ca86bc0e9fe286b45dc6
jkoyasu <jp19957811@outlook.jp>
2022-01-24 19:36:54 +0900

    threadView調整

--- walking/Communication/PostView.swift
+++ walking/Communication/PostView.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class PostView: UIViewController {
 
+    @IBOutlet weak var postText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func post(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    
 }
