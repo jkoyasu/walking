9f06863c3b52174d1c052f536a132fe0bd535674
yousukeHasegawa <hasegawa.yousuke.mn@mitsubishichem-sys.co.jp>
2022-01-25 11:36:49 +0900

    20220125_マージ&Delegateファイル更新

--- walking/AppDelegate.swift
+++ walking/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import YammerSDK
+import MSAL
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -34,7 +35,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        return MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String)
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+            
+            guard let urlContext = URLContexts.first else {
+                return
+            }
+            
+            let url = urlContext.url
+            let sourceApp = urlContext.options.sourceApplication
+            
+            MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: sourceApp)
+    }
 
 }
 
