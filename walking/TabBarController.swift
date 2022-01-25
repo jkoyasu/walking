94f0ee420bbc320f14e34b81dc41aa2e7928f75c
jkoyasu <jp19957811@outlook.jp>
2022-01-25 17:52:04 +0900

    RecordView編集

--- walking/TabBarController.swift
+++ walking/TabBarController.swift
@@ -10,10 +10,19 @@ import YammerSDK
 import MSAL
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        self.delegate = self
     }
-    
+ 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+ 
+    func tabBarController(tabBarController: UITabBarController, didSelectViewController viewController: UIViewController) {
+        if viewController is UITabBarDelegate {
+            let v = viewController as! UITabBarDelegate
+        }
+    }
 }
