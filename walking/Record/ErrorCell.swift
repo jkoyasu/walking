84834e434f19ac06910516fc101a3a4eea011054
jkoyasu <jp19957811@outlook.jp>
2022-01-28 19:32:54 +0900

    Record実装、Calender Extension追加

---
+++ walking/Record/ErrorCell.swift
@@ -0,0 +1,23 @@
+//
+//  ErrorCell.swift
+//  walking
+//
+//  Created by koyasu on 2022/01/28.
+//
+
+import UIKit
+
+class ErrorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- walking/Record/PersonRecord.swift
+++ walking/Record/PersonRecord.swift
@@ -1,10 +1,3 @@
-//
-//  PersonRecord.swift
-//  walking
-//
-//  Created by koyasu on 2022/01/26.
-//
-
 import Foundation
 
 struct PersonRecord:Codable{
@@ -30,14 +23,46 @@ struct PersonContent:Codable{
 }
 
 struct PersonRankingList:Codable{
-    let term:String?
+    @StringForcible var term:String?
     let ranking:[PersonRanking]
 }
 
 struct PersonRanking:Codable{
-    let term:String?
+    @StringForcible var term:String?
     let rank:Int
     let mail:String?
     let name:String?
     let steps:Int
+}
+
+@propertyWrapper
+struct StringForcible: Codable {
+    
+    var wrappedValue: String?
+    
+    enum CodingKeys: CodingKey {}
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let string = try? container.decode(String.self) {
+            wrappedValue = string
+        } else if let integer = try? container.decode(Int.self) {
+            wrappedValue = "\(integer)"
+        } else if let double = try? container.decode(Double.self) {
+            wrappedValue = "\(double)"
+        } else if container.decodeNil() {
+            wrappedValue = nil
+        }
+        else {
+            throw DecodingError.typeMismatch(String.self, .init(codingPath: container.codingPath, debugDescription: "Could not decode incoming value to String. It is not a type of String, Int or Double."))
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
+    }
+    init() {
+        self.wrappedValue = nil
+    }
 }
